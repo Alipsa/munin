bde5dd69abf69772ed5fdaff9e4ac1577b256e4f
pernyf <per.nyfelt@nordnet.se>
2020-11-05 12:56:19 +0100

    add admin page

--- src/main/java/se/alipsa/renjin/webreports/controller/AdminController.java
+++ src/main/java/se/alipsa/renjin/webreports/controller/AdminController.java
@@ -1,4 +1,13 @@
 package se.alipsa.renjin.webreports.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminController {
+
+  @GetMapping("/admin/index")
+  public String adminIndex() {
+    return "adminIndex";
+  }
 }
