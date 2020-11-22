64aaeaa161970413dce7f3b72451de984f348563
per <per@alipsa.se>
2020-11-23 00:14:43 +0100

    change back to spring boot as parent

--- src/test/java/se/alipsa/munin/SpringBootStartupTestIT.java
+++ src/test/java/se/alipsa/munin/SpringBootStartupTestIT.java
@@ -1,4 +1,4 @@
-package test.alipsa.munin;
+package se.alipsa.munin;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import se.alipsa.munin.Application;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class)
