2f468163fe9430a90a4cadf06e74fd413532ab28
per <per@alipsa.se>
2020-12-26 14:39:26 +0100

    iris reports example

---
+++ src/test/resources/pieChartWithExternalImage.R
@@ -0,0 +1,25 @@
+library('se.alipsa:htmlcreator')
+
+html.clear()
+html.add("<html><body>")
+html.add("<h2>Iris report</h2>")
+
+if(exists("inout")) {
+  html.add(html.imgUrl(paste0("file://", getwd(),"/resources/iris.jpg")))
+} else {
+  html.add(html.imgUrl("/common/iris.jpg"))
+}
+species <- table(iris$Species)
+html.add(
+  pie, 
+  species, 
+  labels = paste(names(species), "\n", species, sep=""), 
+  main="Pie Chart of Species\n (with sample sizes)"
+)
+
+html.add("</html></body>")
+
+if(exists("inout")) {
+  inout$viewHtml(html.content(), "Pie chart")
+}
+html.content()
