8440d51020deb352e9d622f64ad07df25197e55e
per <per@alipsa.se>
2020-12-29 14:22:22 +0100

    remove unnecessary semicolon

--- src/bin/passwordEncoder.groovy
+++ src/bin/passwordEncoder.groovy
@@ -7,6 +7,6 @@
 @Grab(group='org.springframework.security', module='spring-security-core', version='5.4.1')
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
-def pwd = System.console().readPassword("Enter password to encode: ");
+def pwd = System.console().readPassword("Enter password to encode: ")
 def encPwd = new BCryptPasswordEncoder().encode(String.valueOf(pwd))
 println("Encoded password is: $encPwd")
