7a4b197fae49ea4da8212e0e9fa7a9535e0b202a
per <per@alipsa.se>
2021-03-13 20:44:30 +0100

    release 1.1.3

--- src/main/java/se/alipsa/munin/service/ReportEngine.java
+++ src/main/java/se/alipsa/munin/service/ReportEngine.java
@@ -8,7 +8,6 @@ import org.renjin.sexp.StringArrayVector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import se.alipsa.munin.util.EnvironmentUtil;
 import se.alipsa.renjin.starter.RenjinSessionEnginePool;
