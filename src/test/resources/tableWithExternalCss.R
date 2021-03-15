f8a39fc79315e1dd8087daf9b8bcb16fac2f9f19
pernyf <per.nyfelt@nordnet.se>
2021-03-15 09:44:11 +0100

    locator works for jquery 3.6.0 so simplifying the header (and fix the upgrade to 4.6.0 bug in the process)

--- src/test/resources/tableWithExternalCss.R
+++ src/test/resources/tableWithExternalCss.R
@@ -5,8 +5,8 @@ html.add("<html><head>")
 if(exists("inout")) {
   # To be able to preview it in Ride:
   html.add("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'>")
-  html.add("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>")
-  html.add("<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js'></script>")
+  html.add("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>")
+  html.add("<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.min.js'></script>")
   html.add(paste0("<link rel='stylesheet' href='file://", getwd(), "/resources/mystyle.css'>"))
 } else {
   html.add("<link rel='stylesheet' href='/common/mystyle.css'>")
